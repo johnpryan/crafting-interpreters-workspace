4e5de7e51c13ca3e63b0c13500065ed439fc1501
John Ryan <john.p.ryan4@gmail.com>
2020-06-28 08:50:12 -0700

    13.3 -> end of chapter

--- jlox/src/com/craftinginterpreters/lox/AstPrinter.java
+++ jlox/src/com/craftinginterpreters/lox/AstPrinter.java
@@ -27,6 +27,11 @@ class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitSuperExpr(Expr.Super expr) {
+        return null;
+    }
+
+    @Override
     public String visitThisExpr(Expr.This expr) {
         return null;
     }
