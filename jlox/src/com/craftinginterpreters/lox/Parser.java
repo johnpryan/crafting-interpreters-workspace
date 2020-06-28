62c63ee41c0a365a98c080907549091b3006d491
John Ryan <john.p.ryan4@gmail.com>
2020-06-28 07:48:42 -0700

    13 -> 13.3 add inheritance (without ability to call superclass methods)

--- jlox/src/com/craftinginterpreters/lox/Parser.java
+++ jlox/src/com/craftinginterpreters/lox/Parser.java
@@ -47,6 +47,12 @@ public class Parser {
         Token name = consume(IDENTIFIER, "Expect class name.");
         consume(LEFT_BRACE, "Expect '{' before class body.");
 
+        Expr.Variable superclass = null;
+        if (match(LESS)) {
+            consume(IDENTIFIER, "Expect superclass name.");
+            superclass = new Expr.Variable(previous());
+        }
+
         List<Stmt.Function> methods = new ArrayList<>();
         while (!check(RIGHT_BRACE) && !isAtEnd()) {
             methods.add(function("method"));
@@ -54,7 +60,7 @@ public class Parser {
 
         consume(RIGHT_BRACE, "Expect '}' after class body.");
 
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     private Stmt statement() {
