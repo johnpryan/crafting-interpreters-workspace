c064d94f8ffe45c1e0b2d90e72f4fe68d0d8d954
John Ryan <john.p.ryan4@gmail.com>
2020-07-01 12:06:24 -0700

    16.1.1 -> 16.3

--- clox/vm.c
+++ clox/vm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include "compiler.h"
 #include "common.h"
 #include "debug.h"
 #include "vm.h"
@@ -76,8 +77,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
