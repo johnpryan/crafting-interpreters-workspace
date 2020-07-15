f35708e5c326dc8a0e7fa5ff803a16b6fa1e479a
John Ryan <john.p.ryan4@gmail.com>
2020-07-15 07:17:06 -0700

    26.4 - sweep()

--- clox/memory.c
+++ clox/memory.c
@@ -143,6 +143,29 @@ static void traceReferences() {
     }
 }
 
+static void sweep() {
+    Obj* previous = NULL;
+    Obj* object = vm.objects;
+    while (object != NULL) {
+        if (object->isMarked) {
+            object->isMarked = false;
+            previous = object;
+            object = object->next;
+        } else {
+            Obj* unreached = object;
+
+            object = object->next;
+            if (previous != NULL) {
+                previous->next = object;
+            } else {
+                vm.objects = object;
+            }
+
+            freeObject(unreached);
+        }
+    }
+}
+
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
     printf("-- gc begin\n");
@@ -150,6 +173,7 @@ void collectGarbage() {
 
     markRoots();
     traceReferences();
+    sweep();
 
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
