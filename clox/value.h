fdaf7dee7ce1a128e4c84daf744d11bc2c4fdfbf
John Ryan <john.p.ryan4@gmail.com>
2020-07-22 08:44:39 -0700

    30 -> 30.3.4

--- clox/value.h
+++ clox/value.h
@@ -1,10 +1,48 @@
 #ifndef clox_value_h
 #define clox_value_h
 
+#include <string.h>
+
 #include "common.h"
 
 typedef struct sObj Obj;
 typedef struct sObjString ObjString;
+
+#ifdef NAN_BOXING
+
+#define QNAN     ((uint64_t)0x7ffc000000000000)
+
+#define TAG_NIL   1 // 01.
+#define TAG_FALSE 2 // 10.
+#define TAG_TRUE  3 // 11.
+
+typedef uint64_t Value;
+
+#define IS_NIL(v)       ((v) == NIL_VAL)
+#define IS_NUMBER(v)    (((v) & QNAN) != QNAN)
+
+#define AS_BOOL(v)      ((v) == TRUE_VAL)
+#define AS_NUMBER(v)    valueToNum(v)
+
+#define BOOL_VAL(b)     ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL       ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL        ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL         ((Value)(uint64_t)(QNAN | TAG_NIL))
+#define NUMBER_VAL(num) numToValue(num)
+
+static inline double valueToNum(Value value) {
+    double num;
+    memcpy(&num, &value, sizeof(Value));
+    return num;
+}
+
+static inline Value numToValue(double num) {
+    Value value;
+    memcpy(&value, &num, sizeof(double));
+    return value;
+}
+
+#else
 
 typedef enum {
     VAL_BOOL,
@@ -35,6 +73,8 @@ typedef struct {
 #define NIL_VAL           ((Value){ VAL_NIL, { .number = 0 } })
 #define NUMBER_VAL(value) ((Value){ VAL_NUMBER, { .number = value } })
 #define OBJ_VAL(object)   ((Value){ VAL_OBJ, { .obj = (Obj*)object } })
+
+#endif
 
 typedef struct {
     int capacity;
